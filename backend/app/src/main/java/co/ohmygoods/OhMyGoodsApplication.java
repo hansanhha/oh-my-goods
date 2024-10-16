3ba44c436962f942011ef06d1045264850cd42b2
hansanhha <kwixs35@gmail.com>
2024-10-16 14:26:27 +0900

    refactor: SpringBootApplication 클래스명 변경

--- backend/app/src/main/java/co/ohmygoods/OhMyGoodsApplication.java
+++ backend/app/src/main/java/co/ohmygoods/OhMyGoodsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StartUp {
+public class OhMyGoodsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StartUp.class, args);
+        SpringApplication.run(OhMyGoodsApplication.class, args);
     }
 }
