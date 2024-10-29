10eac5b8210b14c98686da462cdc0ca45487d9ab
hansanhha <kwixs35@gmail.com>
2024-10-29 17:00:05 +0900

    refactor(account): JsonAuthenticationEntryPoint, JsonAccessDeniedHandler 수정

--- backend/app/src/main/java/co/ohmygoods/auth/jwt/JsonAuthenticationEntryPoint.java
+++ backend/app/src/main/java/co/ohmygoods/auth/jwt/JsonAuthenticationEntryPoint.java
@@ -1,11 +1,10 @@
 package co.ohmygoods.auth.jwt;
 
-import co.ohmygoods.global.exception.UnauthorizedException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -13,22 +12,19 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.Instant;
 
 @Component
 public class JsonAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        if (authException instanceof UnauthorizedException e) {
-            var objectMapper = new ObjectMapper();
+        var objectMapper = new ObjectMapper();
 
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding(StandardCharsets.UTF_8.displayName());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.displayName());
 
-            var errorResponse = FilterErrorResponse.of(e.getMessage(), e.getTimestamp());
-            objectMapper.writeValue(response.getWriter(), errorResponse);
-        }
+        response.getWriter().write(objectMapper.writeValueAsString(authException.getMessage()));
+        response.flushBuffer();
     }
 }
