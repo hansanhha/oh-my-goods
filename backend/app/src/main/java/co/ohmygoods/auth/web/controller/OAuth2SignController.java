1aaefdaebac464ef9a49eab715bb7e5fb3ee07d5
hansanhha <kwixs35@gmail.com>
2024-10-30 18:13:51 +0900

    refactor(account): OAuth2SignController RequestMapping 경로 수정

--- backend/app/src/main/java/co/ohmygoods/auth/web/controller/OAuth2SignController.java
+++ backend/app/src/main/java/co/ohmygoods/auth/web/controller/OAuth2SignController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/account")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class OAuth2SignController {
 
