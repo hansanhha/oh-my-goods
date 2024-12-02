a2af0d462ce46a150e85c9e659790f907284ef6f
hansanhha <kwixs35@gmail.com>
2024-12-02 19:42:26 +0900

    coupoon 발행 임시

--- backend/app/src/main/java/co/ohmygoods/coupon/service/CouponService.java
+++ backend/app/src/main/java/co/ohmygoods/coupon/service/CouponService.java
@@ -1,16 +1,27 @@
 package co.ohmygoods.coupon.service;
 
-import co.ohmygoods.coupon.repository.CouponAccountHistoryRepository;
+import co.ohmygoods.auth.account.entity.OAuth2Account;
+import co.ohmygoods.auth.account.repository.AccountRepository;
+import co.ohmygoods.coupon.exception.CouponException;
+import co.ohmygoods.coupon.model.entity.Coupon;
+import co.ohmygoods.coupon.model.entity.CouponIssuanceHistory;
+import co.ohmygoods.coupon.repository.CouponIssuanceHistoryRepository;
+import co.ohmygoods.coupon.repository.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class CouponService {
 
-    private final CouponAccountHistoryRepository couponUsageHistoryRepository;
+    private final CouponValidationService couponValidationService;
+    private final CouponRepository couponRepository;
+    private final AccountRepository accountRepository;
+    private final CouponIssuanceHistoryRepository couponIssuanceHistoryRepository;
 
     /*
     todo coupon
@@ -19,5 +30,11 @@ public class CouponService {
      - 쿠폰 보유 내역 api
      */
 
-    public void
+    public void issueCoupon(Long couponId, String accountEmail) {
+        Coupon coupon = couponRepository.findById(couponId).orElseThrow(CouponException::notFoundCoupon);
+        OAuth2Account account = accountRepository.findByEmail(accountEmail).orElseThrow(CouponException::notFoundAccount);
+        List<CouponIssuanceHistory> couponAccountHistories = couponIssuanceHistoryRepository.findAllByCouponAndAccount(coupon, account);
+
+        couponValidationService.validateBeforeIssue(coupon, account, couponAccountHistories.size());
+    }
 }
