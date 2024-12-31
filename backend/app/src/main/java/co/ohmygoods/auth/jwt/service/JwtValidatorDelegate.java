76bf510c31ce7a47dd9d6270aa25ba2b7f0d0708
hansanhha <kwixs35@gmail.com>
2024-12-31 16:10:47 +0900

    refactor(auth): JwtValidatorDelegate 유효성 검증 로직 수정

--- backend/app/src/main/java/co/ohmygoods/auth/jwt/service/JwtValidatorDelegate.java
+++ backend/app/src/main/java/co/ohmygoods/auth/jwt/service/JwtValidatorDelegate.java
@@ -1,6 +1,5 @@
 package co.ohmygoods.auth.jwt.service;
 
-import co.ohmygoods.auth.exception.AuthError;
 import co.ohmygoods.auth.jwt.service.dto.JwtValidationResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
@@ -10,12 +9,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * <p>jwt 라이브러리 별 모든 검증 구현체에게 string token에 대한 검증을 위임하는 객체</p>
+ * <p>유효하지 않은 토큰 판정 조건: 모든 검증 구현체에서 파싱을 실패하거나 유효한 토큰 판정을 내린 적이 없는 경우</p>
+ * <p>유효한 토큰 판정 조건: 특정 검증 구현체에서 파싱과 검증에 모두 성공한 경우</p>
+ */
 @Primary
 @Component
 @RequiredArgsConstructor
 public class JwtValidatorDelegate implements JwtValidator {
 
-    private static final boolean ALL_VALIDATORS_INVALID_RESULT = false;
     private static final boolean VALID_RESULT = true;
 
     private final List<JwtValidator> validators;
@@ -26,20 +29,25 @@ public class JwtValidatorDelegate implements JwtValidator {
                 .map(validator -> validator.validate(token))
                 .toList();
 
-        Map<Boolean, List<JwtValidationResult>> groupedResults = results.stream().collect(Collectors.groupingBy(JwtValidationResult::isValid));
+        List<JwtValidationResult> parsedValidationResult = getParsedValidationResult(results);
 
-        if (results.stream().allMatch(JwtValidationResult::isParseFailed)) {
+        if (parsedValidationResult.isEmpty()) {
             return JwtValidationResult.parseFailure();
         }
 
-        if (groupedResults.isEmpty()) {
-            return JwtValidationResult.invalid(AuthError.INVALID_JWT);
-        }
+        Map<Boolean, List<JwtValidationResult>> grouping = groupingByValidAmongParsed(parsedValidationResult);
 
-        List<JwtValidationResult> invalidResults = groupedResults.get(ALL_VALIDATORS_INVALID_RESULT);
-
-        return invalidResults.isEmpty()
-                ? groupedResults.get(VALID_RESULT).getFirst()
-                : invalidResults.getFirst();
+        return grouping.get(VALID_RESULT).isEmpty()
+            ? grouping.get(!VALID_RESULT).getFirst()
+            : grouping.get(VALID_RESULT).getFirst();
     }
+
+    private List<JwtValidationResult> getParsedValidationResult(List<JwtValidationResult> results) {
+        return results.stream().filter(result -> !result.isParseFailed()).toList();
+    }
+
+    private Map<Boolean, List<JwtValidationResult>> groupingByValidAmongParsed(List<JwtValidationResult> results) {
+        return results.stream().collect(Collectors.groupingBy(JwtValidationResult::isValid));
+    }
+
 }
