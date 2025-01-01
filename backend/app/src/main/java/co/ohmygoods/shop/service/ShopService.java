496947ff8ba6f20bc3e9bd10a2441b30c2415c79
hansanhha <kwixs35@gmail.com>
2025-01-01 19:23:13 +0900

    feat(auth): ShopController 추가

--- backend/app/src/main/java/co/ohmygoods/shop/service/ShopService.java
+++ backend/app/src/main/java/co/ohmygoods/shop/service/ShopService.java
@@ -1,6 +1,42 @@
 package co.ohmygoods.shop.service;
 
-public interface ShopService {
+import co.ohmygoods.product.repository.ProductCustomCategoryRepository;
+import co.ohmygoods.product.repository.ProductRepository;
+import co.ohmygoods.product.model.entity.ProductCustomCategory;
+import co.ohmygoods.shop.repository.ShopRepository;
+import co.ohmygoods.shop.service.dto.ShopOverviewResponse;
+import co.ohmygoods.shop.exception.ShopNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.stream.Collectors;
 
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ShopService {
+
+    private final ShopRepository shopRepository;
+    private final ProductRepository productRepository;
+    private final ProductCustomCategoryRepository productCustomCategoryRepository;
+
+    public ShopOverviewResponse getShopOverview(Long shopId) {
+        var shop = shopRepository.findById(shopId)
+                .orElseThrow(() -> new ShopNotFoundException(shopId.toString()));
+        var productCategoriesMap = productCustomCategoryRepository.findAllByShop(shop).stream()
+                .collect(Collectors.toMap(ProductCustomCategory::getId, ProductCustomCategory::getCustomCategoryName));
+        var products = productRepository.findAll(Pageable.ofSize(20));
+
+        return ShopOverviewResponse.builder()
+                .name(shop.getName())
+                .introduction(shop.getIntroduction())
+                .createdAt(shop.getCreatedAt())
+                .shopImage(shop.getShopImageName().concat(shop.getIntroduction()))
+                .status(shop.getStatus())
+                .shopAllProductCategories(productCategoriesMap)
+                .products(products.getContent())
+                .build();
+    }
 }
