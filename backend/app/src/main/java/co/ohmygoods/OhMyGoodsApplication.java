3c6921033c1c40e4585e6e4f86f26168381df409
hansanhha <kwixs35@gmail.com>
2025-03-05 13:14:41 +0900

    chore: 주석 및 파일 정리

--- backend/app/src/main/java/co/ohmygoods/OhMyGoodsApplication.java
+++ backend/app/src/main/java/co/ohmygoods/OhMyGoodsApplication.java
@@ -5,28 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-/**
- * todo coupon 도메인, 서비스 구현
- * todo file 도메인, 서비스 구현 --
- * todo review 도메인 구현
- * todo order service 구현
- * todo payment service 리팩토링
- * todo seller 도메인, 서비스 리팩토링
- * todo community 도메인 구현
- * todo auth 권한 기능 수정
- * todo seller 쿠폰 발급 대상 지정 및 쿠폰 발급 로직 수정
- * todo 정산 도메인 설계 및 구현
- * todo 모든 도메인 컨트롤러, 멱등성(필요 부분), 에러 처리
- * todo 유닛/통합 테스트
- * todo 캐싱(레디스)
- * todo 로깅, api 문서화
- * todo docker
- * todo aws
- * todo ci,cd
- * todo cars 패턴 적용
- * todo ddd 및 port/adapter 패턴 적용(새 브랜치)
- * todo 프론트
- */
 @SpringBootApplication
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
